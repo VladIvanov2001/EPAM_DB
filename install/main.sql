0c1060dce4061f02de2ec4355bbbdd6977cf02db
VladIvanov2001 <45943204+VladIvanov2001@users.noreply.github.com>
2020-05-09 17:16:55 +0300

    update

--- install/main.sql
+++ install/main.sql
@@ -5,4 +5,5 @@ DEFINE TBS_LOCATION = &&2
 SPOOL 'install\INSTALL_SCHEMA.log'
 @install\create_tablespaces.sql &&USER_NAME &&TBS_LOCATION
 @install\create_user.sql &&USER_NAME
+@install\grants.sql &&USER_NAME
 SPOOL OFF
--- upgrade/upgrade001/main.sql
+++ upgrade/upgrade001/main.sql
@@ -3,7 +3,6 @@ SET TIMING ON
 SPOOL UPGRADE_SCHEMA.LOG
 
 DEFINE USER_NAME = &&1
-DEFINE TABLESPACE_NAME = &&2
 
 SET SERVEROUTPUT ON
 PROMPT Username to upgrade: &&USER_NAME
@@ -12,17 +11,17 @@ connect &&USER_NAME/oracle
 -- Run our scripts
 
 PROMPT Calling create_tables started
-@create/create_user.sql &&2
-@create/create_album.sql &&2
-@create/create_playlist.sql &&2
-@create/create_composition.sql &&2
-@create/compositions_review.sql &&2
-@create/album_review.sql &&2
-@create/genre.sql &&2
-@create/playlist_contain.sql &&2
-@create/story_of_listening.sql &&2
-@create/user_playlist.sql &&2
-@create/user_state.sql &&2
+@create/create_user.sql &&1
+@create/create_album.sql &&1
+@create/create_playlist.sql &&1
+@create/create_composition.sql &&1
+@create/compositions_review.sql &&1
+@create/album_review.sql &&1
+@create/genre.sql &&1
+@create/playlist_contain.sql &&1
+@create/story_of_listening.sql &&1
+@create/user_playlist.sql &&1
+@create/user_state.sql &&1
 
 
 
